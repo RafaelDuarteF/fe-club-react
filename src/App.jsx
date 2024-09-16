9935c8639a143c56ef1cb5ee7ddcb05c85a075e9
Guilherme Oliveira <sguioliveira@gmail.com>
2024-09-16 01:13:33 -0300

    Slides adicionados

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
 import './App.css';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from 'react-router-dom';
 import Home from './pages/Home';
 import Sobre from './pages/Sobre/Sobre';
 import LoginPage from './pages/Login/Login';
+import Corridas from './pages/Corridas/Corridas';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-    <Route path="/" element={<Home />} />
-    <Route path="/sobre" element={<Sobre />} /> 
-    <Route path="/Login" element={<LoginPage />} />
-    </>
-    
-  )
+      <Route path="/" element={<Home />} />
+      <Route path="/sobre" element={<Sobre />} />
+      <Route path="/Login" element={<LoginPage />} />
+      <Route path="/corridas" element={<Corridas />} />
+    </>,
+  ),
 );
 
 export default function App() {
-  return (
-    <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router} />;
 }
