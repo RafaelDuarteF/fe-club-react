22c5fb3f34e58b71b683ae13046975a6d6e84943
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-17 20:59:50 -0300

    Pagina cadastro criada

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import './App.css';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements } from 'react-router-dom';
-import Home from './pages/Home';
-import Sobre from './pages/Sobre/Sobre';
+
+import CadastroPage from './pages/Cadastro/Cadastro';
+import Home from './pages/Home/Home';
 import LoginPage from './pages/Login/Login';
+import Sobre from './pages/Sobre/Sobre';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-    <Route path="/" element={<Home />} />
-    <Route path="/sobre" element={<Sobre />} /> 
-    <Route path="/Login" element={<LoginPage />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/sobre" element={<Sobre />} />
+      <Route path="/Login" element={<LoginPage />} />
+      <Route path="/Cadastro" element={<CadastroPage />} />
+
     </>
-    
+
   )
 );
 
