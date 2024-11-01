01d4c70ccb80e71112c53e650954fae1f847b932
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-01 20:43:51 -0300

    começado nova tela de perfil

--- src/pages/Perfil/Perfil.jsx
+++ src/pages/Perfil/Perfil.jsx
@@ -41,67 +41,32 @@ export default function Perfil() {
       <main>
         <section className="seu-carro">
           <div className="left">
-            <span className="title">SEU CARRO</span>
-            <img src={carro} alt="Seu carro" />
-            <h3>GERAÇÃO I</h3>
-          </div>
-          <div className="right">
-            <ul>
-              <li>Modelo: Gen1</li>
-              <li>Velocidade Máxima: 310km/h</li>
-              <li>Motor: v8</li>
-            </ul>
-            <button className="btn-editar">EDITAR</button>
-          </div>
-        </section>
-        <section className="time-pilotos">
-          <div className="left-time-pilotos">
-            <span className="title-left">Seu Time</span>
-            <span className="name-team">Los Corridas</span>
-            <img src={fotoTime} alt="Brasão do seu time" className="brasao-time" />
-            <button className="btn-editar-time">EDITAR</button>
-            <span className="points-team">1432 pontos</span>
-          </div>
-          <div className="right-time-pilotos">
-            <div className="container-right">
-              <span className="title-right">Seus Pilotos</span>
-              <div className="container-pilotos">
-                <div className="piloto">
-                  <span className="nome-piloto">Nick Cassidy</span>
-                  <img src={piloto1} alt="Seu piloto 1" />
-                </div>
-                <div className="piloto">
-                  <span className="nome-piloto">Edoardo Mortara</span>
-                  <img src={piloto2} alt="Seu piloto 2" />
+            <div className="info-escudo">
+              <div className='container-brasao'>
+                <img src={fotoTime} alt="Brasão do seu time" className="brasao-time" />
+                <div className="info-time">
+                  <span>Monteiro's Club</span>
+                  <h2>823 Coins</h2>
                 </div>
               </div>
+              <div className="container-carro">
+                <img src={carro} alt="Seu Carro"/>
+                <span className=''>Lv2</span>
+                <button className="btn-editar">Editar</button>
+              </div>
+
             </div>
-            <button className="btn-editar-pilotos">EDITAR PILOTOS</button>
+            <div className="container-ranking">
+              <h3>Ranking</h3>
 
-            {/* Renderiza o aviso somente se o mercado não estiver fechado */}
-            { !timeLeft.includes('FECHADO') && (
-              <>
-                <span className="aviso-mercado" style={{ color: timeLeft.includes('FECHADO') ? 'red' : 'black' }}>
-                  O mercado fecha em {timeLeft}
-                </span>
+              
+            </div>
+          </div>
+          <div className="right">
 
-                <span className="aviso-mercado-mobile" style={{ color: timeLeft.includes('FECHADO') ? 'red' : 'black' }}>
-                  O mercado fecha em 
-                </span>
-                <span className="aviso-mercado-mobile" style={{ color: timeLeft.includes('FECHADO') ? 'red' : 'black' }}>
-                  {timeLeft}
-                </span>
-              </>
-            )}
-
-            {/* Renderiza a mensagem "O MERCADO ESTÁ FECHADO!" se estiver fechado */}
-            { timeLeft.includes('FECHADO') && (
-              <span className="aviso-mercado-fechado" style={{ color: 'red' }}>
-                {timeLeft}
-              </span>
-            )}
           </div>
         </section>
+       
       </main>
     
     </>
